ca16220b7cf67e48ae8d5d2c1716c449c057964b
CalebHarsh <caleb.harsh139@gmail.com>
2018-01-13 01:22:18 -0700

    added assets and js folders and some javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,43 @@
-//am pm
+//===============================================================================================
+//getting data from the users from the website
 var registerVolunteer = function () {
   var name = $("#vol-name")
   var email = $("#vol-email")
   var phone = $("#vol-phone")
+  var region = $("#vol-region")
   var times = {
-    sunday: [$("#sun-morn"), $("#sun-after")],
     monday: [$("#mon-morn"), $("#mon-after")],
     tuesday: [$("#tues-morn"), $("#tues-after")],
     wednesday: [$("#wed-morn"), $("#wed-after")],
     thursday: [$("#thurs-morn"), $("#thurs-after")],
-    friday: [$("#fri-morn"), $("#fri-after")],
-    saturday: [$("#sat-morn"), $("#sat-after")]
+    friday: [$("#fri-morn"), $("#fri-after")]
+    //Not avaliale on weekends
+    // saturday: [$("#sat-morn"), $("#sat-after")],
+    // sunday: [$("#sun-morn"), $("#sun-after")]
   }
   return {
     name: name,
     email: email,
     phone: phone,
+    region: region,
     times: times
   }
 }
+
+var askForRide = function () {
+  var region = $("#apl-region")
+  var email = $("#apl-email")
+  var date = $("#apl-date")
+  var timeOfDay = $("#apl-time")
+
+  return {
+    email: email,
+    region: region,
+    date: date,
+    timeOfDay: timeOfDay
+  }
+}
+
+// getting data from the users from the website
+//====================================================================================
+//validate info 
